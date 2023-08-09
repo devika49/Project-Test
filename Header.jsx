14b284896cbfd86648450d8f71d56f99236f5943
devika49 <126885754+devika49@users.noreply.github.com>
2023-08-09 23:55:01 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,17 @@
+import "./header.css"
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span class Name="headerTitleSm"> React & Node</span>
+        <span className="headerTitleLg">Blog</span>
+        </div>
+        <img
+           className="headerImg"
+           src="https://cdn.pixabay.com/photo/2015/05/12/09/13/freelancer-763730_1280.jpg"
+           alt=""
+           />
+    </div>
+  )
+}
